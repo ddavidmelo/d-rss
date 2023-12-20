8ccfaf76ccec410e5d0e3046f136375fbe3e7d87
dm <ddavidmeloo@gmail.com>
2023-12-20 23:33:44 +0000

    start_url with /d-rss and base with /d-rss

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
@@ -9,39 +9,41 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      mode: "development",
-      includeAssets: ["/favicon.ico"],
+      mode: 'development',
+      includeAssets: ['/favicon.ico'],
       strategies: 'injectManifest',
       srcDir: 'public',
       filename: 'firebase-messaging-sw.js',
+      base: '/d-rss',
       manifest: {
-        name: "D-RSS",
-        short_name: "D-RSS",
-        theme_color: "#06091c",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#06091c",
+        name: 'D-RSS',
+        short_name: 'D-RSS',
+        theme_color: '#06091c',
+        start_url: '/d-rss',
+        // start_url: '/',
+        display: 'standalone',
+        background_color: '#06091c',
         icons: [
           {
-            src: "logo-48-48.png",
-            sizes: "48x48",
-            type: "image/png",
+            src: 'logo-48-48.png',
+            sizes: '48x48',
+            type: 'image/png',
           },
           {
-            src: "logo-96-96.png",
-            sizes: "96x96",
-            type: "image/png",
+            src: 'logo-96-96.png',
+            sizes: '96x96',
+            type: 'image/png',
           },
           {
-            src: "logo-512-512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: 'logo-512-512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
           {
-            src: "logo-512-512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
+            src: 'logo-512-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
           },
         ],
       },
